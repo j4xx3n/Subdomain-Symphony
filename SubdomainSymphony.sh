25ff0b4d121caff12bafc9c79d61581c1453b406
j4xx3n <105862349+j4xx3n@users.noreply.github.com>
2024-06-19 07:19:00 -0500

    Update SubdomainSymphony.sh

--- SubdomainSymphony.sh
+++ SubdomainSymphony.sh
@@ -107,7 +107,7 @@ activeScan() {
 
   # Fuzz for subdomains with ffuf and add json to file
   ffuf -w subdomains-top1million-5000.txt -u https://FUZZ.$domain -o fuzz.json
-  jq -r '.results[].url' fuzz.json | tee subdomains  # Parse subdomains from ffuf json file
+  jq -r '.results[].url' fuzz.json | tee -a subdomains  # Parse subdomains from ffuf json file
 }
 
 
